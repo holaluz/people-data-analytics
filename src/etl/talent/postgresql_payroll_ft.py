b3aaa0af7ad7ea2353f6d87f299defdf4235a184
angelabholaluz <angela.belencoso@holaluz.com>
2022-08-10 15:54:12 +0200

    Update postgresql_payroll_ft.py

--- src/etl/talent/postgresql_payroll_ft.py
+++ src/etl/talent/postgresql_payroll_ft.py
@@ -1,30 +1,25 @@
+from msilib import schema
 import requests
 import os 
 import sys
 import json
+import pandas as pd
+from holaluz_datatools.sql import PostgreSQLClient
+from holaluz_datatools.credentials import load_credentials
 sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), '..','..','utils')))
-sys.path.append(os.path.join(os.environ['USERPROFILE'], 'documents', 'github', 'people-data-analytics', 'src', 'utils'))
-print(sys.path)
+#sys.path.append(os.path.join(os.environ['USERPROFILE'], 'documents', 'github', 'people-data-analytics', 'src', 'utils'))
 from refresh_token_factorial import get_token
-json.loads(response.decode('utf-8'))[0]
 
+SCHEMA = "people"
+TABLE_NAME = "OPS_PAYROLL_FT"
 
 token = get_token()
 url = "https://api.factorialhr.com/api/v1/payroll/contract_versions"
 
-payload={}
 headers = {'Authorization': f'Bearer {token}'}
-print(headers)
+response = requests.request("GET", url, headers=headers)
 
-response = requests.request("GET", url, headers=headers, data=payload)
-response_text = response.json()
-
-response.content
-
-from datetime import date
-import pandas as pd
-df = pd.DataFrame(json.loads(response.text))
-df.columns
+df = pd.DataFrame(response.json())
 df.drop(columns=['has_payroll','salary_frequency','es_has_teleworking_contract', 'es_cotization_group',
 'es_contract_observations','es_job_description','es_working_day_type_id',
 'es_education_level_id', 'es_professional_category_id',
@@ -32,9 +27,6 @@ df.drop(columns=['has_payroll','salary_frequency','es_has_teleworking_contract',
 'fr_coefficient', 'fr_contract_type_id', 'fr_level_id', 'fr_step_id',
 'fr_mutual_id', 'fr_professional_category_id', 'fr_work_type_id',
 'de_contract_type_id'], inplace=True)
-
-from holaluz_datatools.sql import PostgreSQLClient
-from holaluz_datatools.credentials import load_credentials
 
 postgresql_client = PostgreSQLClient(**load_credentials('people_write'), lazy_initialization = True)
 postgresql_client.write_table(
@@ -44,7 +36,5 @@ postgresql_client.write_table(
     if_exists = 'replace' # see the different values that if_exists can take in the method docsting
 )
 
-#df = pd.DataFrame.from_dict(data, orient='index')
-df.head()
 
 
