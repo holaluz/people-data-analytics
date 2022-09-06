df7b331e20046cc5c90a835d0b7a4140b1b6f982
angelabholaluz <angela.belencoso@holaluz.com>
2022-09-06 09:42:20 +0200

    Update postgresql_employees_ft.py

--- src/etl/comm/postgresql_employees_ft.py
+++ src/etl/comm/postgresql_employees_ft.py
@@ -61,20 +61,7 @@ df1['employee_ids'] = df1['employee_ids'].astype('int64')
 df2.drop(columns=['id_y','employee_ids', 'lead_ids','avatar','company_holiday_ids','location_id','manager_id','hiring',], inplace=True)
 df2. rename(columns = {'id_x':'id','name': 'team_name'}, inplace = True)
 
-#Cross ids from employees & contract version endpoint from api factorial to bring job_titles to employee table
-
-"""postgresql_client = PostgreSQLClient(**load_credentials('people_write'), lazy_initialization = True)
-df_master = []
-query_master = """ """select a."Id Req > DNI/NIE", a."Status", a."Job title"
-from people.people."PPL_EMPLOYEES_FT" b
-left join people."temp"."OPS_MASTER_FT" a on a."Id Req > DNI/NIE" = b.factorial_id"""
-
-""" for chunk in postgresql_client.make_query(query_master, chunksize=160000):
-    df_master.append(chunk)
-df_masterfile = pd.concat(df_master, ignore_index=True)
-postgresql_client.close_connection() 
-
-df3 = pd.merge(df2, df_masterfile, how='left', left_on='id', right_on='id req > dni/nie') """
+#Load new df into table ppl_employees
 
 #credentials = load_credentials(credentials_fp = 'C:/Users/Administrator/creds/creds_people.yml')
 with open(os.path.join('C:/Users/Administrator/creds', 'creds_people.yml')) as file:
