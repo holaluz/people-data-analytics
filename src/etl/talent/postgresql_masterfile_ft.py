10ee97476b5cfa4cf5f34f0beed115831f22bcba
angelabholaluz <angela.belencoso@holaluz.com>
2022-09-07 11:25:35 +0200

    [FIX]Updated creds

--- src/etl/talent/postgresql_masterfile_ft.py
+++ src/etl/talent/postgresql_masterfile_ft.py
@@ -1,10 +1,15 @@
 import os 
+import sys
 import gspread
+import yaml
 import pandas as pd
 from genericpath import exists
 from holaluz_datatools.sql import PostgreSQLClient
 from holaluz_datatools.credentials import load_credentials
 from holaluz_datatools.credentials import load_google_drive_service_account_credentials
+
+#sys.path.append(os.path.join(os.environ['USERPROFILE'], 'documents', 'github', 'people-data-analytics', 'src', 'utils'))
+#print(sys.path)
 
 #1.Request access to the google sheet with json credentials
 
@@ -17,7 +22,7 @@ sheet_credentials = load_google_drive_service_account_credentials(
 
 gspread_client = gspread.authorize(sheet_credentials)
 
-sh = gspread_client.open('Master File_2022')
+sh = gspread_client.open('Copia de Master File_2022')
 ws = sh.worksheet("Budget 2022")
 list_cols = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]
 df = pd.DataFrame()
@@ -26,11 +31,19 @@ for col in list_cols:
     df_master = pd.DataFrame.from_dict(value)
     df = pd.concat([df, df_master], axis=1)
 
+"""sh = gspread_client.open('Copia de Master File_2022')
+ws = sh.worksheet("Budget 2022")
+rows = ws.get_all_records() 
+df = pd.DataFrame.from_dict(rows)"""
+
 df.columns = df.iloc[0]
 df = df[1:]
 df.columns
+print(df)
 
-postgre_client = PostgreSQLClient(**load_credentials('people_write'), lazy_initialization = True)
+with open(os.path.join('C:/Users/Administrator/creds', 'creds_people.yml')) as file:
+    credentials = yaml.load(file, Loader=yaml.FullLoader)
+postgre_client = PostgreSQLClient(**credentials['people_write'], lazy_initialization = True)
 postgre_client.write_table(
     df, 
     "OPS_MASTER_FT", 
