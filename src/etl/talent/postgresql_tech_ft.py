29f9f41436b06df17dd0dc58811e47c58e767c60
angelabholaluz <angela.belencoso@holaluz.com>
2022-10-20 10:30:37 +0200

    [NEW]Create postgre tech_staff table

---
+++ src/etl/talent/postgresql_tech_ft.py
@@ -0,0 +1,37 @@
+import os 
+import gspread
+import pandas as pd
+import yaml
+from genericpath import exists
+from holaluz_datatools.sql import PostgreSQLClient
+from holaluz_datatools.credentials import load_credentials
+from holaluz_datatools.credentials import load_google_drive_service_account_credentials
+
+#1.Request access to the google sheet with json credentials
+
+LOCAL_CREDS_PATH = os.path.join(os.environ['USERPROFILE'], 'creds')
+DRIVE_CREDS_FILENAME = 'drive_to_python.json'
+
+sheet_credentials = load_google_drive_service_account_credentials(
+    credentials_fp=os.path.join(LOCAL_CREDS_PATH, DRIVE_CREDS_FILENAME)
+)
+
+gspread_client = gspread.authorize(sheet_credentials)
+
+sh = gspread_client.open(' Tech_Master File_2022')
+ws = sh.worksheet("Budget 2022")
+rows = ws.get_values() 
+df_ws = pd.DataFrame.from_dict(rows)
+df = df_ws.iloc[:,0:25]
+df.columns= df.iloc[0,:] #remove numerical headers
+df = df.iloc[1:,:]
+
+with open(os.path.join('C:/Users/Administrator/creds', 'creds_people.yml')) as file:
+    credentials = yaml.load(file, Loader=yaml.FullLoader)
+postgre_client = PostgreSQLClient(**credentials['people_write'], lazy_initialization = True)
+postgre_client.write_table(
+    df, 
+    "TAL_TECH_FT", 
+    "temp", 
+    if_exists = 'replace' # see the different values that if_exists can take in the method docsting
+)
