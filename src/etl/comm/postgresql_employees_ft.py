fc1f90d5e429eefd4433a445ba149f1a719597e1
angelabholaluz <angela.belencoso@holaluz.com>
2022-11-08 11:59:11 +0100

    [FIX]updated initial load

--- src/etl/comm/postgresql_employees_ft.py
+++ src/etl/comm/postgresql_employees_ft.py
@@ -24,9 +24,11 @@ headers = {'Authorization': f'Bearer {token}'}
 response = requests.request("GET", url, headers=headers)
 
 df_sociedad = pd.DataFrame(response.json())
-columns = list(df_sociedad.columns)
 
+"""columns = list(df_sociedad.columns)
 print(df_sociedad)
+print(f"Distinct entries: {len(df_sociedad.groupby(['id']))}")"""
+
 
 columns = list(df_sociedad.columns)
 columns_to_delete_ls = []
@@ -64,31 +66,31 @@ dict_sociedad_master = {'holaluz_clidom':'Holaluz Clidom', 'clidom_solar':'Clido
 df_total=df_total.replace({'sociedad_id': dict_sociedad_master})
 df_total.rename(columns = {'sociedad_id':'sociedad_name'}, inplace = True)
 
-#Fill split column with split values
-
-
-#Join split from split table
-
+#credentials = load_credentials(credentials_fp = 'C:/Users/Administrator/creds/creds_people.yml')
+with open(os.path.join('C:/Users/Administrator/creds', 'creds_people.yml')) as file:
+    credentials = yaml.load(file, Loader=yaml.FullLoader)
 postgresql_client = PostgreSQLClient(**credentials['people_write'], lazy_initialization = True)
-df_split = []
-query_split = """select a.split, a.team, b.id  from people.people."OPS_SPLIT_FT" a
-join people.people."PPL_EMPLOYEES_FT" b 
-on a.team = b.team_name """
-
-for chunk in postgresql_client.make_query(query_split, chunksize=160000):
-    df_split.append(chunk)
-df_employees = pd.concat(df_split, ignore_index=True)
-postgresql_client.close_connection()
-
-df_merge= pd.merge(df_total, df_employees, how= 'left', on= 'id')
-
-df_merge.drop(columns=['team'], inplace=True)
-
-
-postgresql_client = PostgreSQLClient(**credentials['people_write'], lazy_initialization = True)
-df_merge.to_csv('C:/Users/Administrator/Desktop/output.csv')
+df_total.to_csv('C:/Users/Administrator/Desktop/output.csv')
 import psycopg2
 credentials_postgre = credentials['people_write']
+m_dbCon = psycopg2.connect(user=credentials_postgre['username'], password=credentials_postgre['password'], host=credentials_postgre['host'] 
+,database=credentials_postgre['database'])
+curr = m_dbCon.cursor()
+curr.execute('truncate table "people"."PPL_EMPLOYEES_FT_NEW"')
+curr.close()
+m_dbCon.commit()
+postgresql_client.write_table(
+    df_total,
+    "PPL_EMPLOYEES_FT", 
+    "people", 
+    if_exists = 'append' # see the different values that if_exists can take in the method docsting
+)
+
+
+"""postgresql_client = PostgreSQLClient(**credentials['people_write'], lazy_initialization = True)
+df_total.to_csv(os.path.join(os.environ['USERPROFILE'], 'output.csv'))
+credentials_postgre = credentials['people_write']
+import psycopg2
 m_dbCon = psycopg2.connect(user=credentials_postgre['username'], password=credentials_postgre['password'], host=credentials_postgre['host'] 
 ,database=credentials_postgre['database'])
 curr = m_dbCon.cursor()
@@ -96,13 +98,8 @@ curr.execute('truncate table "people"."PPL_EMPLOYEES_FT"')
 curr.close()
 m_dbCon.commit()
 postgresql_client.write_table(
-    df_merge, 
+    df_total, 
     "PPL_EMPLOYEES_FT", 
     "people", 
     if_exists = 'append' # see the different values that if_exists can take in the method docsting
-)
-
-
-
-
-
+)"""
